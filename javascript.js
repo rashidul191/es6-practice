6a8e9970f4864186d7a32ba5540904a40aa08974
rashidul191 <rashidul.191cse.gub@gmail.com>
2021-12-31 12:43:34 +0600

    we know Esc done

--- javascript.js
+++ javascript.js
@@ -1,1 +1,40 @@
-console.log("Hello World")
+// let name = "rashidul ilsma"
+// name = "rased"
+// console.log(name)
+
+
+// function add(num1, num2 = 50){
+//    // num2 = num2 || 10
+//  var result = num1 + num2;
+//  return result;
+// }
+// const sum = add(3);
+// console.log(sum)
+
+
+// const firstName = "Rashiud";
+// const lastName = "Islam";
+// const fullName = firstName +" "+ lastName;
+// const fullName2 = `${firstName} ${lastName} Rased`;
+
+// const multiLine = "I love you\n"
+// + "I miss you\n"
+// + "I need you\n"
+
+// const multiLine2 = `I love you
+// I miss you
+// I need you
+// Baily road a dorkar nai`
+
+// console.log(multiLine2);
+
+// const name = "Rased"
+// const age = 20;
+// const name2 = "Rashidul"
+// const age2 = 10;
+
+// const allInformation = `${name} : ${age}
+// ${name2} : ${age2}
+// all information collacted done`
+
+// console.log(allInformation)
