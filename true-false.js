fd8316be5bea8a182b230941c116fa57049e6a45
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-01-01 10:52:39 +0600

    true and false condition learn

---
+++ true-false.js
@@ -0,0 +1,26 @@
+// Falsy Value
+// 0
+//""
+// false
+// undefined
+// null
+// NaN
+
+
+//Trueth Value
+// 0> any number || 0 < any number
+// "0"
+// " "
+// "false"
+// true
+//{}
+//[]
+
+// const name = "Rashidul"
+let number= NaN;
+console.log(number)
+if(number){
+   console.log("Condition is True");
+}else{
+   console.log("Condition is False")
+}
